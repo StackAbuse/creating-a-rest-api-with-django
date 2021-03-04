69be510f418633c04a2510b80499b248aa184a94
naazneen <jatunazneen@gmail.com>
2021-03-04 11:27:56 +0530

    added files

---
+++ shopping_cart/api_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import CartItem
+
+admin.site.register(CartItem)
---
+++ shopping_cart/api_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class CartItem(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_price = models.FloatField()
+    product_quantity = models.PositiveIntegerField()
